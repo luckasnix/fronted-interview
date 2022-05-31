e0c21ea6670954e748abcc899f35ebc8c85cf2f9
LUCAS ALVES COSTA <lucas.costa.e@riachuelo.com.br>
2022-05-31 15:10:37 -0300

    feat: add test of Lucas Costa

--- stages-teste/src/Child.js
+++ stages-teste/src/Child.js
@@ -1,20 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Box, Loader, getTheme } from "styled-minimal";
 
-const Child = ({ color = "yellow" }) => {
+const Child = ({ index, color = "yellow", duration, onExpired }) => {
+  // Este estado denota se o tempo foi ou não expirado
+  const [isExpired, setIsExpired] = useState(false);
   const { colors } = getTheme();
+
+  // Contador de tempo baseado na prop "duration" recebida
+  useEffect(() => {
+    const durationTimer = setTimeout(() => {
+      setIsExpired(true);
+      onExpired(index);
+    }, duration);
+
+    // Quando desmontado, o contador de tempo deve ser limpo
+    return () => {
+      clearTimeout(durationTimer);
+    };
+  }, []);
 
   return (
     <Box
       alignItems="center"
-      bg="transparent"
+      bg={isExpired ? colors[color] : 'transparent'}
       border={`2px solid ${colors[color]}`}
       borderRadius="8px"
+      marginBottom="8px"
       display="flex"
       height={128}
       justifyContent="center"
     >
-      <Loader color={colors[color]} size={48} />
+      {!isExpired && <Loader color={colors[color]} size={48} />}
     </Box>
   );
 };
--- stages-teste/src/Parent.js
+++ stages-teste/src/Parent.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Container, Heading } from "styled-minimal";
+import data from './data.json';
 import "./styles.css";
 
 import Child from "./Child";
 
 export default function Parent() {
+  // Armazena o array a ser trabalhado
+  const [stepsArr, setStepsArr] = useState([]);
+  const [message, setMessage] = useState(null);
+
+  // Gera um novo array na primeira renderização
+  // Este novo array possui estrutura adequada à feature
+  useEffect(() => {
+    setStepsArr(data.steps.map(({ key, value }, index) => ({
+      key,
+      color: value,
+      duration: data.durations[key],
+      shouldRender: index === 0 ? true : false,
+      expired: false,
+    })));
+  }, []);
+
+  // Este callback, com base no Child, atualiza o stepsArr
+  // definindo o próximo componente a ser renderizado
+  const handleExpired = (index) => {
+    setStepsArr((oldState) => oldState.map((curObj, curIndex) => {
+      if (curIndex === index) {
+        return {
+          ...curObj,
+          expired: true,
+        };
+      }
+      if (curIndex === index + 1) {
+        return {
+          ...curObj,
+          shouldRender: true,
+        };
+      }
+      return curObj;
+    }));
+  };
+
+  // Este useEffect assiste mudanças no stepsArr para verificar
+  // se todos os Child renderizaram
+  useEffect(() => {
+    const isDone = stepsArr.every(({ expired }) => expired === true);
+    if (isDone && stepsArr.length > 0) {
+      setMessage(`It's done!`);
+    }
+  }, [stepsArr]);
+
   return (
     <Container>
       <Heading mb={4} textAlign="center">
-        Stages
+        Stages<br/>{message}
       </Heading>
-      <Child />
+      {stepsArr.map(({ key, color, duration, shouldRender }, index) => (
+        shouldRender && (
+          <Child
+            key={key}
+            index={index}
+            color={color}
+            duration={duration}
+            onExpired={handleExpired}
+          />
+        )
+      ))}
     </Container>
   );
 }
